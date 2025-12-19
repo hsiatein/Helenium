4f72587dceab25da99ce2b909846af6873b945eb
Xia Tian <3023625451@qq.com>
2025-12-19 17:38:17 +0800

    一些尝试

--- heleny-server/src/main.rs
+++ heleny-server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use tokio;
+use dotenvy::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    // 读取环境变量
+    dotenv().ok();
+    if let Ok(val)=env::var("MUGI"){
+        println!("MUGI: {}", val);
+    }
     println!("Hello, world!");
 }
